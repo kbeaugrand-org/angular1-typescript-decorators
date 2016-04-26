8480881becc0f51a3d7c572a71399346d4a53b60
Kevin BEAUGRAND <kbeaugrand@ntic-infos.net>
2016-04-26 21:26:24 +0200

    Upate bower package

---
+++ demo/bower_components/angular1-typescript-decorators/typings/tsd.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="angularjs/angular.d.ts" />
+/// <reference path="angularjs/angular-route.d.ts" />
+/// <reference path="urijs/URIjs.d.ts" />
